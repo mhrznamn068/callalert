35421e6dfb0ffcd4dfaafd72dcdce7cd420ce52a
Aman Maharjan <mhrznamn068@gmail.com>
2024-01-04 10:19:51 +0545

    added new files

--- _helpers/run-zabbix-execute.sh
+++ _helpers/run-zabbix-execute.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+<<<<<<< HEAD
 TRIGGER_NAME="Aman is testing Alert"
 TRIGGER_SEVERITY="High"
 
 CALLALERT_URL="http://localhost:5080/callalert"
+=======
+TRIGGER_NAME="Aman is testing Alert)"
+TRIGGER_SEVERITY="High"
+
+CALLALERT_URL="http://172.17.1.9:5080/callalert"
+>>>>>>> django
 
 generate_post_data()
 {
